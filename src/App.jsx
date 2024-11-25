6c7535c15b7d8a1c638084f176f2d416a42d2193
GloGlorGloria <gloriabcit@gmail.com>
2024-11-25 09:40:40 -0800

    link filtering and sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 import Countries from './components/Countries';
-import './App.css'
+import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [originalCountries, setOriginalCountries] = useState([]);
   const [isCheckedPopulation, setIsCheckedPopulation] = useState(false);
   const [isCheckedArea, setIsCheckedArea] = useState(false);
+  const [isCheckedAlpha, setIsCheckedAlpha] = useState(false);
   const [fetchStatus, setFetchStatus] = useState('idle');
   const [continent, setContinent] = useState('All');
   const [subregion, setSubregion] = useState('Choose region');
-  
+
   const isLoading = fetchStatus === 'loading';
   const isError = fetchStatus === 'error';
 
@@ -20,12 +21,10 @@ function App() {
         setFetchStatus('loading');
         const response = await fetch('https://restcountries.com/v3.1/all');
         const data = await response.json();
-        console.log(data);
         setCountries(data);
         setOriginalCountries(data);
         setFetchStatus('idle');
-        console.log(countries);
-      } catch(e){
+      } catch (e) {
         setFetchStatus('error');
         console.error(e.message);
       }
@@ -33,101 +32,114 @@ function App() {
     fetchData();
   }, []);
 
-  function handleAlphaChange(e){
-    console.log(e.target.checked);
+  useEffect(() => {
+    applyFiltersAndSorting();
+  }, [isCheckedAlpha, isCheckedPopulation, isCheckedArea, continent, subregion]);
 
-    if(e.target.checked){
-      setCountries([...countries].sort((a, b) => a.name.common.localeCompare(b.name.common)));
-    } else {
-      setCountries(originalCountries);
-    }
-  }
+  function applyFiltersAndSorting() {
+    let updatedCountries = [...originalCountries];
 
-  function handlePopulationChange(e){
-    console.log(e.target.checked);
-
-    setIsCheckedPopulation(!isCheckedPopulation);
-    if (isCheckedArea) {
-      setIsCheckedArea(!isCheckedArea);
+    if (continent !== 'All') {
+      updatedCountries = updatedCountries.filter(country =>
+        country.continents.includes(continent)
+      );
     }
 
-    if(e.target.checked){
-      setCountries([...originalCountries].sort((a, b) => b.population - a.population).slice(0,10));
-    } else {
-      setCountries(originalCountries);
+    if (subregion !== 'Choose region') {
+      updatedCountries = updatedCountries.filter(
+        country => country.subregion === subregion
+      );
     }
-  }
 
-  function handleAreaChange(e){
-    console.log(e.target.checked);
-
-    setIsCheckedArea(!isCheckedArea);
     if (isCheckedPopulation) {
-      setIsCheckedPopulation(!isCheckedPopulation);
+      updatedCountries = updatedCountries
+        .sort((a, b) => b.population - a.population)
+        .slice(0, 10);
+
+    } else if (isCheckedArea) {
+      updatedCountries = updatedCountries
+        .sort((a, b) => b.area - a.area)
+        .slice(0, 10);
+
+    } else if (isCheckedAlpha) {
+      updatedCountries = updatedCountries.sort((a, b) =>
+        a.name.common.localeCompare(b.name.common)
+      );
     }
 
-    if(e.target.checked){
-      setCountries([...originalCountries].sort((a, b) => b.area - a.area).slice(0,10))
-    } else {
-      setCountries(originalCountries);
+    setCountries(updatedCountries);
+  }
+
+  function handleAlphaChange(e) {
+    setIsCheckedAlpha(e.target.checked);
+    if (e.target.checked) {
+      setIsCheckedPopulation(false);
+      setIsCheckedArea(false);
     }
   }
 
-  function handleContinentChange(e){
+  function handlePopulationChange(e) {
+    setIsCheckedPopulation(e.target.checked);
+    if (e.target.checked) {
+      setIsCheckedAlpha(false);
+      setIsCheckedArea(false);
+    }
+  }
+
+  function handleAreaChange(e) {
+    setIsCheckedArea(e.target.checked);
+    if (e.target.checked) {
+      setIsCheckedAlpha(false);
+      setIsCheckedPopulation(false);
+    }
+  }
+
+  function handleContinentChange(e) {
     setContinent(e.target.value);
     setSubregion('Choose region'); 
-
-    
-    if(e.target.value === 'All'){
-      setCountries(originalCountries);
-    } else {
-      setCountries([...originalCountries].filter(country => country.continents.includes(e.target.value)));
-    }
   }
 
-  function handleSubRegionChange(e){
+  function handleSubRegionChange(e) {
     setSubregion(e.target.value);
-    setContinent('All');
-
-    if(e.target.value === 'Choose region'){
-      setCountries(originalCountries);
-    } else {
-      setCountries([...originalCountries].filter(country => country.subregion === (e.target.value)));
-    }
+    setContinent('All'); 
   }
 
   return (
     <div>
       <h1>Countries of the World</h1>
-    <div>
-    <div className='filter_container'>
+      <div className="filter_container">
         <p>Filter & sort</p>
-        <div className='filterInfo_container'>
-          <div className='filter'>
-            <input type="checkbox" onChange={handleAlphaChange}/> 
+        <div className="filterInfo_container">
+          <div className="filter">
+            <input
+              type="checkbox"
+              onChange={handleAlphaChange}
+              checked={isCheckedAlpha}
+            />
             <label>Alpha</label>
           </div>
 
-          <div className='filter'>
+          <div className="filter">
             <p>Top 10</p>
-            <input 
-              type="checkbox" 
+            <input
+              type="checkbox"
               onChange={handlePopulationChange}
               checked={isCheckedPopulation}
-            />by population
-            <br/>
-            <input 
-              type="checkbox" 
+            />
+            by population
+            <br />
+            <input
+              type="checkbox"
               onChange={handleAreaChange}
               checked={isCheckedArea}
-            /> 
+            />
             <label>by area</label>
           </div>
 
-          <div className='filter'>
+          <div className="filter">
             <p>By continent</p>
             <select value={continent} onChange={handleContinentChange}>
-              <option defaultValue="All">All</option>
+              <option value="All">All</option>
               <option value="Antarctica">Antarctica</option>
               <option value="Asia">Asia</option>
               <option value="Africa">Africa</option>
@@ -138,10 +150,10 @@ function App() {
             </select>
           </div>
 
-          <div className='filter'>
+          <div className="filter">
             <p>By subregion</p>
             <select value={subregion} onChange={handleSubRegionChange}>
-              <option defaultValue="Choose region">Choose region</option>
+              <option value="Choose region">Choose region</option>
               <option value="Caribbean">Caribbean</option>
               <option value="Western Europe">Western Europe</option>
               <option value="Western Africa">Western Africa</option>
@@ -163,31 +175,20 @@ function App() {
               <option value="Central Asia">Central Asia</option>
               <option value="Southern Asia">Southern Asia</option>
               <option value="South America">South America</option>
-              <option value="Australia and New Zealand">Australia and New Zealand</option>
+              <option value="Australia and New Zealand">
+                Australia and New Zealand
+              </option>
               <option value="Central America">Central America</option>
               <option value="Eastern Europe">Eastern Europe</option>
             </select>
-           </div>
+          </div>
         </div>
-      <div>
-
       </div>
-    </div>
-   
-     
-    </div>
-
-    
-
-  
-
-    
-
 
       {isLoading ? (
         <p>Loading data...</p>
       ) : isError ? (
-        <p>Opps sorry! Error in fetching data</p>
+        <p>Oops sorry! Error in fetching data</p>
       ) : (
         <Countries data={countries} />
       )}
