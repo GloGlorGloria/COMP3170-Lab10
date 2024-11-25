176ef05688065e95473d4ea512a624352286e8b5
GloGlorGloria <gloriabcit@gmail.com>
2024-11-25 12:10:09 -0800

    delink filter and sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useEffect, useState } from 'react';
 import Countries from './components/Countries';
-import './App.css';
+import './App.css'
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [originalCountries, setOriginalCountries] = useState([]);
   const [isCheckedPopulation, setIsCheckedPopulation] = useState(false);
   const [isCheckedArea, setIsCheckedArea] = useState(false);
-  const [isCheckedAlpha, setIsCheckedAlpha] = useState(false);
   const [fetchStatus, setFetchStatus] = useState('idle');
   const [continent, setContinent] = useState('All');
   const [subregion, setSubregion] = useState('Choose region');
-
+  
   const isLoading = fetchStatus === 'loading';
   const isError = fetchStatus === 'error';
 
@@ -21,10 +20,12 @@ function App() {
         setFetchStatus('loading');
         const response = await fetch('https://restcountries.com/v3.1/all');
         const data = await response.json();
+        console.log(data);
         setCountries(data);
         setOriginalCountries(data);
         setFetchStatus('idle');
-      } catch (e) {
+        console.log(countries);
+      } catch(e){
         setFetchStatus('error');
         console.error(e.message);
       }
@@ -32,118 +33,101 @@ function App() {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    if (originalCountries.length > 0) {
-      applyFiltersAndSorting();
+  function handleAlphaChange(e){
+    console.log(e.target.checked);
+
+    if(e.target.checked){
+      setCountries([...countries].sort((a, b) => a.name.common.localeCompare(b.name.common)));
+    } else {
+      setCountries(originalCountries);
     }
-  }, [isCheckedAlpha, isCheckedPopulation, isCheckedArea, continent, subregion]);
+  }
 
-  function applyFiltersAndSorting() {
-    if (originalCountries.length === 0) return;
-    let updatedCountries = [...originalCountries];
-    
+  function handlePopulationChange(e){
+    console.log(e.target.checked);
 
-    if (continent !== 'All') {
-      updatedCountries = updatedCountries.filter(country =>
-        country.continents.includes(continent)
-      );
-    }
-
-    if (subregion !== 'Choose region') {
-      updatedCountries = updatedCountries.filter(
-        country => country.subregion === subregion
-      );
+    setIsCheckedPopulation(!isCheckedPopulation);
+    if (isCheckedArea) {
+      setIsCheckedArea(!isCheckedArea);
     }
 
+    if(e.target.checked){
+      setCountries([...originalCountries].sort((a, b) => b.population - a.population).slice(0,10));
+    } else {
+      setCountries(originalCountries);
+    }
+  }
+
+  function handleAreaChange(e){
+    console.log(e.target.checked);
+
+    setIsCheckedArea(!isCheckedArea);
     if (isCheckedPopulation) {
-      updatedCountries = updatedCountries
-        .sort((a, b) => b.population - a.population)
-        .slice(0, 10);
-
-    } else if (isCheckedArea) {
-      updatedCountries = updatedCountries
-        .sort((a, b) => b.area - a.area)
-        .slice(0, 10);
-
-    } else if (isCheckedAlpha) {
-      updatedCountries = updatedCountries.sort((a, b) =>
-        a.name.common.localeCompare(b.name.common)
-      );
+      setIsCheckedPopulation(!isCheckedPopulation);
     }
 
-    setCountries(updatedCountries);
-  }
-
-  function handleAlphaChange(e) {
-    setIsCheckedAlpha(e.target.checked);
-    if (e.target.checked) {
-      setIsCheckedPopulation(false);
-      setIsCheckedArea(false);
+    if(e.target.checked){
+      setCountries([...originalCountries].sort((a, b) => b.area - a.area).slice(0,10))
+    } else {
+      setCountries(originalCountries);
     }
   }
 
-  function handlePopulationChange(e) {
-    setIsCheckedPopulation(e.target.checked);
-    if (e.target.checked) {
-      setIsCheckedAlpha(false);
-      setIsCheckedArea(false);
-    }
-  }
-
-  function handleAreaChange(e) {
-    setIsCheckedArea(e.target.checked);
-    if (e.target.checked) {
-      setIsCheckedAlpha(false);
-      setIsCheckedPopulation(false);
-    }
-  }
-
-  function handleContinentChange(e) {
+  function handleContinentChange(e){
     setContinent(e.target.value);
     setSubregion('Choose region'); 
+
+    
+    if(e.target.value === 'All'){
+      setCountries(originalCountries);
+    } else {
+      setCountries([...originalCountries].filter(country => country.continents.includes(e.target.value)));
+    }
   }
 
-  function handleSubRegionChange(e) {
+  function handleSubRegionChange(e){
     setSubregion(e.target.value);
-    setContinent('All'); 
+    setContinent('All');
+
+    if(e.target.value === 'Choose region'){
+      setCountries(originalCountries);
+    } else {
+      setCountries([...originalCountries].filter(country => country.subregion === (e.target.value)));
+    }
   }
 
   return (
     <div>
       <h1>Countries of the World</h1>
-      <div className="filter_container">
+    <div>
+    <div className='filter_container'>
         <p>Filter & sort</p>
-        <div className="filterInfo_container">
-          <div className="filter">
-            <input
-              type="checkbox"
-              onChange={handleAlphaChange}
-              checked={isCheckedAlpha}
-            />
+        <div className='filterInfo_container'>
+          <div className='filter'>
+            <input type="checkbox" onChange={handleAlphaChange}/> 
             <label>Alpha</label>
           </div>
 
-          <div className="filter">
+          <div className='filter'>
             <p>Top 10</p>
-            <input
-              type="checkbox"
+            <input 
+              type="checkbox" 
               onChange={handlePopulationChange}
               checked={isCheckedPopulation}
-            />
-            by population
-            <br />
-            <input
-              type="checkbox"
+            />by population
+            <br/>
+            <input 
+              type="checkbox" 
               onChange={handleAreaChange}
               checked={isCheckedArea}
-            />
+            /> 
             <label>by area</label>
           </div>
 
-          <div className="filter">
+          <div className='filter'>
             <p>By continent</p>
             <select value={continent} onChange={handleContinentChange}>
-              <option value="All">All</option>
+              <option defaultValue="All">All</option>
               <option value="Antarctica">Antarctica</option>
               <option value="Asia">Asia</option>
               <option value="Africa">Africa</option>
@@ -154,10 +138,10 @@ function App() {
             </select>
           </div>
 
-          <div className="filter">
+          <div className='filter'>
             <p>By subregion</p>
             <select value={subregion} onChange={handleSubRegionChange}>
-              <option value="Choose region">Choose region</option>
+              <option defaultValue="Choose region">Choose region</option>
               <option value="Caribbean">Caribbean</option>
               <option value="Western Europe">Western Europe</option>
               <option value="Western Africa">Western Africa</option>
@@ -179,20 +163,31 @@ function App() {
               <option value="Central Asia">Central Asia</option>
               <option value="Southern Asia">Southern Asia</option>
               <option value="South America">South America</option>
-              <option value="Australia and New Zealand">
-                Australia and New Zealand
-              </option>
+              <option value="Australia and New Zealand">Australia and New Zealand</option>
               <option value="Central America">Central America</option>
               <option value="Eastern Europe">Eastern Europe</option>
             </select>
-          </div>
+           </div>
         </div>
+      <div>
+
       </div>
+    </div>
+   
+     
+    </div>
+
+    
+
+  
+
+    
+
 
       {isLoading ? (
         <p>Loading data...</p>
       ) : isError ? (
-        <p>Oops sorry! Error in fetching data</p>
+        <p>Opps sorry! Error in fetching data</p>
       ) : (
         <Countries data={countries} />
       )}
